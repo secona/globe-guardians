3d4359aa24b022fd856ec175cf9e48a497b45026
gracekarinn <gracekarina12@gmail.com>
2024-10-05 15:48:31 +0700

    Refactor game configuration for better physics simulation

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,11 +1,6 @@
 import { Scene } from "phaser";
 import { Player } from "../sprites/player";
 
-interface PlayerAnims {
-  pressedCursor: "up" | "down" | "left" | "right";
-  actionAnimation: string;
-}
-
 export class Game extends Scene {
   private player!: Player;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
@@ -14,48 +9,59 @@ export class Game extends Scene {
     super("Game");
   }
 
-  init() {}
+  init() {
+    console.log("Game scene initialized");
+  }
 
   preload() {
     this.load.path = "assets/";
-    // Make sure to load your player sprite here
+    console.log("Loading farmer sprite...");
     this.load.spritesheet("farmer", "farmer.png", {
-      frameWidth: 32,
-      frameHeight: 32,
+      frameWidth: 35,
+      frameHeight: 48,
     });
   }
 
   create() {
+    console.log("Creating player...");
     this.player = new Player({
       scene: this,
       x: 100,
       y: 100,
       texture: "farmer",
     });
+    console.log("Player created", this.player);
 
-    this.cursors = this.input?.keyboard?.createCursorKeys()!;
+    this.cursors = this.input.keyboard?.createCursorKeys()!;
+
+    // Add any additional setup for your game scene here
+    // For example, creating the world, adding other game objects, etc.
   }
 
   update() {
-    const direction = new Phaser.Math.Vector2(0, 0);
+    if (this.player && this.cursors) {
+      const direction = new Phaser.Math.Vector2(0, 0);
 
-    if (this.cursors.left.isDown) {
-      direction.x = -1;
-    } else if (this.cursors.right.isDown) {
-      direction.x = 1;
+      if (this.cursors.left.isDown) {
+        direction.x = -1;
+      } else if (this.cursors.right.isDown) {
+        direction.x = 1;
+      }
+
+      if (this.cursors.up.isDown) {
+        direction.y = -1;
+      } else if (this.cursors.down.isDown) {
+        direction.y = 1;
+      }
+
+      if (direction.x !== 0 || direction.y !== 0) {
+        direction.normalize();
+        this.player.move(direction);
+      } else {
+        this.player.stopMoving();
+      }
     }
 
-    if (this.cursors.up.isDown) {
-      direction.y = -1;
-    } else if (this.cursors.down.isDown) {
-      direction.y = 1;
-    }
-
-    if (direction.x !== 0 || direction.y !== 0) {
-      direction.normalize();
-      this.player.move(direction);
-    } else {
-      this.player.stopMoving();
-    }
+    // Add any additional update logic for your game scene here
   }
 }
