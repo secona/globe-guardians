6f6dc91bdfbbc230c93ef7631cd5bc2bfc7b4863
gracekarinn <gracekarina12@gmail.com>
2024-10-05 22:22:03 +0700

    feat: Add sawit planting functionality to the game scene

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -8,9 +8,11 @@ export class Game extends Scene {
   private player!: Player;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   private plantKey!: Phaser.Input.Keyboard.Key;
+  private sawitKey!: Phaser.Input.Keyboard.Key;
   private colliders!: Physics.Arcade.StaticGroup;
   private plantableArea!: Phaser.Geom.Rectangle;
   private plantedCrops: Phaser.GameObjects.Image[] = [];
+  private plantedSawit: Phaser.GameObjects.Image[] = [];
   private tree: Phaser.GameObjects.Image[] = [];
   private notification: Notification | null = null;
   private isModalOpen: boolean = false;
@@ -42,17 +44,64 @@ export class Game extends Scene {
     this.load.image("nanem", "nanem.png");
     this.load.image("pohon", "pohon.png");
     this.load.image("peat", "peat.png");
+    this.load.image("sawit", "sawit.png");
   }
 
   create() {
     const bg = this.add.image(0, 0, "bg").setOrigin(0, 0);
     this.physics.world.setBounds(0, 0, bg.width, bg.height);
-    this.add.image(700, 230, "pohon").setOrigin(0, 0);
-    this.add.image(630, 220, "pohon").setOrigin(0, 0);
-    this.add.image(650, 270, "pohon").setOrigin(0, 0);
-    this.add.image(604, 127, "peat").setOrigin(0, 0);
-    this.add.image(596, 95, "peat").setOrigin(0, 0);
-    this.add.image(628, 108, "peat").setOrigin(0, 0);
+    this.tree = [];
+
+    const treeGridSize = 50;
+    const treeAreaStartX = 600;
+    const treeAreaStartY = 100;
+    const treeAreaWidth = 250;
+    const treeAreaHeight = 200;
+
+    for (
+      let x = treeAreaStartX;
+      x < treeAreaStartX + treeAreaWidth;
+      x += treeGridSize
+    ) {
+      for (
+        let y = treeAreaStartY;
+        y < treeAreaStartY + treeAreaHeight;
+        y += treeGridSize
+      ) {
+        if (Math.random() < 0.7) {
+          const offsetX = Math.random() * 20 - 10;
+          const offsetY = Math.random() * 20 - 10;
+          const tree = this.add
+            .image(x + offsetX, y + offsetY, "pohon")
+            .setOrigin(0, 0);
+          this.tree.push(tree);
+        }
+      }
+    }
+
+    const peatGridSize = 30;
+    const peatAreaStartX = 400;
+    const peatAreaStartY = 80;
+    const peatAreaWidth = 160;
+    const peatAreaHeight = 80;
+
+    for (
+      let x = peatAreaStartX;
+      x < peatAreaStartX + peatAreaWidth;
+      x += peatGridSize
+    ) {
+      for (
+        let y = peatAreaStartY;
+        y < peatAreaStartY + peatAreaHeight;
+        y += peatGridSize
+      ) {
+        if (Math.random() < 0.6) {
+          const offsetX = Math.random() * 10 - 5;
+          const offsetY = Math.random() * 10 - 5;
+          this.add.image(x + offsetX, y + offsetY, "peat").setOrigin(0, 0);
+        }
+      }
+    }
 
     this.player = new Player({
       scene: this,
@@ -69,6 +118,8 @@ export class Game extends Scene {
     }) as Phaser.Types.Input.Keyboard.CursorKeys;
 
     this.plantKey = this.input.keyboard?.addKey(Input.Keyboard.KeyCodes.ONE!)!;
+
+    this.sawitKey = this.input.keyboard?.addKey(Input.Keyboard.KeyCodes.TWO!)!;
 
     this.createColliders();
     this.physics.add.collider(this.player, this.colliders);
@@ -116,7 +167,64 @@ export class Game extends Scene {
         console.log(this.toolState.getSelectedTool());
         this.tryPlant();
       }
+
+      if (this.toolState.getSelectedTool() === 3) {
+        this.tryCutTree();
+      }
+      if (this.toolState.getSelectedTool() === 2) {
+        this.handlePlanting();
+      }
     }
+  }
+
+  private tryCutTree() {
+    const nearestTree = this.getNearestTree();
+
+    if (nearestTree && this.isPlayerNearTree(nearestTree)) {
+      nearestTree.destroy();
+      console.log("Tree cut down!");
+
+      this.tree = this.tree.filter((tree) => tree !== nearestTree);
+    } else {
+      console.log("No tree to cut here!");
+    }
+  }
+
+  private handlePlanting() {
+    if (Phaser.Input.Keyboard.JustDown(this.sawitKey)) {
+      this.tryPlantSawit();
+    }
+  }
+
+  private getNearestTree(): Phaser.GameObjects.Image | null {
+    let nearestTree = null;
+    let minDistance = Infinity;
+
+    this.tree.forEach((tree) => {
+      const distance = Phaser.Math.Distance.Between(
+        this.player.x,
+        this.player.y,
+        tree.x,
+        tree.y
+      );
+
+      if (distance < minDistance) {
+        minDistance = distance;
+        nearestTree = tree;
+      }
+    });
+
+    return nearestTree;
+  }
+
+  private isPlayerNearTree(tree: Phaser.GameObjects.Image): boolean {
+    const distance = Phaser.Math.Distance.Between(
+      this.player.x,
+      this.player.y,
+      tree.x,
+      tree.y
+    );
+    return distance < 50;
   }
 
   private createColliders() {
@@ -151,6 +259,23 @@ export class Game extends Scene {
       console.log("This spot is already occupied!");
     } else {
       console.log("Cannot plant here!");
+    }
+  }
+
+  private tryPlantSawit() {
+    if (
+      this.isPlayerOverPlantableArea() &&
+      !this.isSpotOccupied(this.player.x, this.player.y)
+    ) {
+      const sawit = this.add
+        .image(this.player.x, this.player.y, "sawit")
+        .setOrigin(0.5, 1);
+      this.plantedSawit.push(sawit);
+      console.log("Sawit planted!");
+    } else if (this.isSpotOccupied(this.player.x, this.player.y)) {
+      console.log("This spot is already occupied!");
+    } else {
+      console.log("Cannot plant sawit here!");
     }
   }
 
