be8c0c43a7f6136a303dd58cfd59528049d61826
gracekarinn <gracekarina12@gmail.com>
2024-10-05 17:51:10 +0700

    feat: implement background

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,13 +1,16 @@
-import { Input, Scene } from "phaser";
+import { Input, Scene, Physics } from "phaser";
 import { Player } from "../sprites/player";
 
 export class Game extends Scene {
   private player!: Player;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private colliders!: Physics.Arcade.StaticGroup;
 
   constructor() {
     super("Game");
   }
+
+  init() {}
 
   preload() {
     this.load.path = "assets/";
@@ -15,13 +18,18 @@ export class Game extends Scene {
       frameWidth: 35,
       frameHeight: 48,
     });
+    this.load.image("bg", "farm.png");
   }
 
   create() {
+    const bg = this.add.image(0, 0, "bg").setOrigin(0, 0);
+
+    this.physics.world.setBounds(0, 0, bg.width, bg.height);
+
     this.player = new Player({
       scene: this,
-      x: 100,
-      y: 100,
+      x: 300,
+      y: 400,
       texture: "farmer",
     });
 
@@ -31,14 +39,19 @@ export class Game extends Scene {
       down: Input.Keyboard.KeyCodes.S,
       right: Input.Keyboard.KeyCodes.D,
     }) as Phaser.Types.Input.Keyboard.CursorKeys;
+
+    this.createColliders();
+    this.physics.add.collider(this.player, this.colliders);
+    this.cameras.main.startFollow(this.player);
+    this.cameras.main.setBounds(0, 0, bg.width, bg.height);
+    this.cameras.main.setZoom(1.5);
   }
 
   update() {
     if (this.player && this.cursors) {
       const direction = new Phaser.Math.Vector2(0, 0);
-
-      direction.x = +this.cursors.right.isDown - 2 * +this.cursors.left.isDown;
-      direction.y = +this.cursors.down.isDown - 2 * +this.cursors.up.isDown;
+      direction.x = +this.cursors.right.isDown - +this.cursors.left.isDown;
+      direction.y = +this.cursors.down.isDown - +this.cursors.up.isDown;
       direction.normalize();
 
       if (direction.x !== 0 || direction.y !== 0) {
@@ -47,5 +60,13 @@ export class Game extends Scene {
         this.player.stopMoving();
       }
     }
+  }
+
+  private createColliders() {
+    this.colliders = this.physics.add.staticGroup();
+
+    this.colliders.add(
+      this.add.rectangle(0, 0, 120, 120, 0x000000, 0).setOrigin(0, 0)
+    );
   }
 }
