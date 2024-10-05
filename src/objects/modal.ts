8638842a83c45e1d9986d7389f0b29a12662cb3d
gracekarinn <gracekarina12@gmail.com>
2024-10-05 21:43:29 +0700

    refactor: add links

--- src/objects/modal.ts
+++ src/objects/modal.ts
@@ -9,7 +9,6 @@ export class Notification extends Phaser.GameObjects.Container {
   private exitButton: Phaser.GameObjects.Text;
 
   constructor(scene: Scene) {
-    // Center the modal by setting the container's position at half the scene's width and height
     super(scene, scene.scale.width / 2, scene.scale.height / 2);
 
     this.modalBackground = this.createModalBackground();
@@ -19,13 +18,12 @@ export class Notification extends Phaser.GameObjects.Container {
     this.button = this.createButton();
     this.exitButton = this.createExitButton();
 
-    this.setDepth(1000); // Ensures the modal stays on top
+    this.setDepth(1000);
     scene.add.existing(this);
 
     this.setSize(scene.scale.width, scene.scale.height);
     this.setInteractive();
 
-    // Prevent propagation of pointer events to underlying objects
     this.on(
       "pointerdown",
       (
@@ -40,7 +38,6 @@ export class Notification extends Phaser.GameObjects.Container {
   }
 
   private createModalBackground(): Phaser.GameObjects.Rectangle {
-    // Semi-transparent background covering the whole screen
     const bg = this.scene.add.rectangle(
       0,
       0,
@@ -49,12 +46,12 @@ export class Notification extends Phaser.GameObjects.Container {
       0x000000,
       0.7
     );
+    bg.setOrigin(0.5, 0.5);
     this.add(bg);
     return bg;
   }
 
   private createContentBackground(): Phaser.GameObjects.Rectangle {
-    // Content background that will be centered within the modal
     const bg = this.scene.add.rectangle(0, 0, 300, 200, 0xfff3c6);
     bg.setStrokeStyle(4, 0xc27f1a);
     this.add(bg);
@@ -62,19 +59,17 @@ export class Notification extends Phaser.GameObjects.Container {
   }
 
   private createTitle(): Phaser.GameObjects.Text {
-    // Title text
     const title = this.scene.add.text(0, -70, "Notification", {
       fontSize: "24px",
       color: "#B83C3C",
       fontStyle: "bold",
     });
-    title.setOrigin(0.5); // Center the title
+    title.setOrigin(0.5);
     this.add(title);
     return title;
   }
 
   private createMessage(): Phaser.GameObjects.Text {
-    // Message text, centered
     const message = this.scene.add.text(
       0,
       -10,
@@ -85,13 +80,12 @@ export class Notification extends Phaser.GameObjects.Container {
         align: "center",
       }
     );
-    message.setOrigin(0.5); // Center the message
+    message.setOrigin(0.5);
     this.add(message);
     return message;
   }
 
   private createButton(): Phaser.GameObjects.Text {
-    // Button with a link to Google
     const button = this.scene.add.text(0, 50, "Soil Fertility Protocol", {
       fontSize: "16px",
       color: "#FFF3C6",
@@ -100,9 +94,11 @@ export class Notification extends Phaser.GameObjects.Container {
     });
     button.setOrigin(0.5).setInteractive();
 
-    // Add the logic to open www.google.com when clicked
     button.on("pointerdown", () => {
-      window.open("https://www.google.com", "_blank");
+      window.open(
+        "https://www.globe.gov/documents/352961/9e59d0d4-4cf5-4e62-9802-081a61442ef4",
+        "_blank"
+      );
     });
 
     this.add(button);
@@ -110,7 +106,6 @@ export class Notification extends Phaser.GameObjects.Container {
   }
 
   private createExitButton(): Phaser.GameObjects.Text {
-    // Exit button
     const exitBtn = this.scene.add.text(140, -90, "X", {
       fontSize: "20px",
       color: "#B83C3C",
@@ -119,14 +114,12 @@ export class Notification extends Phaser.GameObjects.Container {
     });
     exitBtn.setOrigin(0.5).setInteractive();
 
-    // Close the modal when exit button is clicked
     exitBtn.on("pointerdown", () => this.closeNotification());
     this.add(exitBtn);
     return exitBtn;
   }
 
   private closeNotification() {
-    // Emit 'closed' event and destroy the modal
     this.emit("closed");
     this.destroy();
   }
