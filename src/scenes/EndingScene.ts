f5a4dd4a3f5ed79f9ec6fb3609d8b2c696e131ec
gracekarinn <gracekarina12@gmail.com>
2024-10-06 09:36:40 +0700

    feat: Add EndingScene with video playback functionality

---
+++ src/scenes/EndingScene.ts
@@ -0,0 +1,41 @@
+import { Scene } from "phaser";
+
+export class EndingScene extends Scene {
+  private video!: Phaser.GameObjects.Video;
+
+  constructor() {
+    super("EndingScene");
+  }
+
+  preload() {
+    this.load.video("ending", "assets/ending.mp4");
+  }
+
+  create() {
+    const { width, height } = this.scale;
+
+    this.video = this.add.video(width / 2, height / 2, "ending");
+
+    const fitVideo = () => {
+      const videoWidth = this.video.width;
+      const videoHeight = this.video.height;
+
+      const scaleX = width / videoWidth;
+      const scaleY = height / videoHeight;
+
+      const scale = Math.min(scaleX, scaleY);
+
+      this.video.setDisplaySize(videoWidth * scale, videoHeight * scale);
+    };
+
+    this.video.on("loadeddata", fitVideo);
+
+    this.video.play(false);
+
+    this.video.on("complete", () => {
+      this.scene.start("NextScene");
+    });
+
+    this.scale.on("resize", fitVideo);
+  }
+}
